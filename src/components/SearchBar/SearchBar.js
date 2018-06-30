2ad188746e22b66dd841e8a3e60977bb7dfea009
James <nurse.djm@gmail.com>
2018-06-30 17:46:46 -0600

    add visual feedback for the sorting options at the top of the search bar, set the state of sorting options and input elements

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,13 +7,58 @@ const sortByOptions = {
   'Most Reviewed': 'review_count'
 };
 
-
 class SearchBar extends React.Component {
-  renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>
-  });
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+
+
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
+    };
+  }
+
+getSortByClass(sortByOption) {
+  if (this.state.sortBy === sortByOption) {
+    return 'active';
+  }
+    return '';
+}
+
+handleSortByChange(sortByOption) {
+  this.setState({sortBy: sortByOption})
+}
+
+handleTermChange(event) {
+  this.setState({term: event.target.value})
+}
+
+handleLocationChange(event) {
+  this.setState({location: event.target.value})
+}
+
+handleSearch(event) {
+  this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+  event.preventDefault()
+}
+
+renderSortByOptions() {
+  return Object.keys(sortByOptions).map(sortByOption => {
+    let sortByOptionValue = sortByOptions[sortByOption];
+    return <li key={sortByOptionValue} onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+    className={this.getSortByClass(sortByOptionValue)}>{sortByOption}</li>
+});
 };
 
   render () {
@@ -25,11 +70,11 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange}/>
         </div>
        <div className="SearchBar-submit">
-         <a>Lets Go</a>
+         <a onClick={this.handleSearch}>Lets Go</a>
        </div>
      </div>
   );
